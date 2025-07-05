c8b13b235c415de4d6698000cb9968dbbec18a1f
teesh3rt <ilaylevy611@gmail.com>
2025-07-05 12:39:17 +0300

    chore(slight): go over the flake file

--- flake.nix
+++ flake.nix
@@ -1,10 +1,12 @@
 {
-  description = "A very basic flake";
+  description = "Teesh's Configuration Flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     stylix.url = "github:nix-community/stylix/release-25.05";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
   };
