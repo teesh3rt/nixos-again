1c100896641d0d2e8d2d414b50c6fb590c880ea6
teesh3rt <ilaylevy611@gmail.com>
2025-07-06 21:53:23 +0300

    feat: add some keys

--- modules/home/wm/hyprland.nix
+++ modules/home/wm/hyprland.nix
@@ -60,6 +60,16 @@ in
         "${mod} SHIFT, S, exec, ${screenshot}"
         "${mod} SHIFT, P, exit"
         "${mod} SHIFT, D, exec, ${pkgs.mako}/bin/makoctl dismiss -a"
+
+        ", XF86AudioPlay, exec, ${pkgs.playerctl}/bin/playerctl play-pause"
+        ", XF86AudioStop, exec, ${pkgs.playerctl}/bin/playerctl stop"
+        ", XF86AudioNext, exec, ${pkgs.playerctl}/bin/playerctl next"
+        ", XF86AudioPrev, exec, ${pkgs.playerctl}/bin/playerctl previous"
+        ", XF86AudioMute, exec, ${pkgs.pulseaudioFull}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle"
+        ", XF86AudioRaiseVolume, exec, ${pkgs.pulseaudioFull}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%"
+        ", XF86AudioLowerVolume, exec, ${pkgs.pulseaudioFull}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%"
+        ", XF86MonBrightnessUp, exec, ${pkgs.brightnessctl}/bin/brightnessctl set 10%+"
+        ", XF86MonBrightnessDown, exec, ${pkgs.brightnessctl}/bin/brightnessctl set 10%-"
       ] ++ bindAdditions;
       monitor = "eDP-1, 2048x1280@120, 0x0, 1";
       input = {
