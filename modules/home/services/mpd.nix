b6ce22cb1dcf315eede7fc825b8fa62acdf3051d
teesh3rt <ilaylevy611@gmail.com>
2025-07-07 12:04:03 +0300

    feat: add a visualizer to ncmpcpp

--- modules/home/services/mpd.nix
+++ modules/home/services/mpd.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 
 {
   services.mpd = {
@@ -9,12 +9,29 @@
         type "pipewire"
         name "device" # this can be whatever you want
       }
+
+      audio_output {
+        type                    "fifo"
+        name                    "my_fifo"
+        path                    "/tmp/mpd.fifo"
+        format                  "44100:16:2"
+      }
     '';
     network.listenAddress = "any";
     network.startWhenNeeded = true;
   };
 
   programs.ncmpcpp.enable = true;
+  programs.ncmpcpp.package = pkgs.ncmpcpp.override {
+    visualizerSupport = true;
+  };
+  programs.ncmpcpp.settings = {
+    visualizer_data_source = "/tmp/mpd.fifo";
+    visualizer_output_name = "my_fifo";
+    visualizer_in_stereo = "yes";
+    visualizer_type = "spectrum";
+    visualizer_look = "+|";
+  };
 
   services.mpd-mpris.enable = true;
 }
