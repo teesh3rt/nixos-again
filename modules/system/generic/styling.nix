eaf02f51a334cd109945eb0af7a003422224aa50
teesh3rt <ilaylevy611@gmail.com>
2025-07-10 14:52:55 +0300

    feat: make the font jbmono

--- modules/system/generic/styling.nix
+++ modules/system/generic/styling.nix
@@ -1,8 +1,17 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   stylix.enable = true;
   stylix.polarity = "dark";
 
   stylix.targets.qt.enable = false;
+
+  stylix.fonts = {
+    serif = config.stylix.fonts.monospace;
+    sansSerif = config.stylix.fonts.monospace;
+    monospace = {
+      package = pkgs.nerd-fonts.jetbrains-mono;
+      name = "JetBrains Mono Nerd Font";
+    };
+  };
 }
